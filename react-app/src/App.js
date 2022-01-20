37e697144690fc3edfa3b9095e7f58e21c6d6c37
pell13 <peixueying@gmail.com>
2022-01-20 15:59:13 +0900

    Fix : version error

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,21 +1,16 @@
-import './App.css';
-import Buyview from './buy_item';
-import Marketview from './market';
-import Novelview from './novel';
+import "./App.css";
+import Buyview from "./buy_item";
+import Marketview from "./market";
+import Novelview from "./novel";
 
-import {
-  BrowserRouter as Router,
-  HashRouter,
-  Route,
-} from "react-router-dom";
-
+import { BrowserRouter as Router, HashRouter, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <Route path="/main" exact component={Marketview}/>
-      <Route path="/" exact component={Buyview}/>
-      <Route path="/novel" exact component={Novelview}/>
+      <Route path="/main" exact component={Marketview} />
+      <Route path="/" exact component={Buyview} />
+      <Route path="/novel" exact component={Novelview} />
     </Router>
   );
 }
