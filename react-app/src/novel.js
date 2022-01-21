8ff8a6394d06fc30738376f9700bb8c082e69d71
Jonnie Yoon <yoonjeongin@sookmyung.ac.kr>
2022-01-21 14:41:59 +0900

    Add: seperate script file

--- react-app/src/novel.js
+++ react-app/src/novel.js
@@ -1,35 +1,18 @@
 import './Game.css';
-
+import scenario from './scenario.json'
 
 import React, { Component, useEffect, useState } from 'react';
 
-const scenario = [
-    {
-      "name": "김만장",
-      "text": "안녕하세요.닉네임 만장일치 입니다."
-    },
-    {
-      "name": "김만정",
-      "text": "두번쨰 샘플 텍스트입니다."
-    },
-    {
-      "text": "이름이 없는 경우에는 이름 창이 사라졌으면 좋겠어요."
-    },
-    {
-      "name": "개",
-      "text": "왈왈"
-    }
-  ]
 function Novelview(props){
   const [count, setCount] = useState(1);
-  const [name, setName] = useState(scenario[0].name);
-  const [text, setText] = useState(scenario[0].text);
+  const [name, setName] = useState(scenario.intro[0].name);
+  const [text, setText] = useState(scenario.intro[0].text);
 
   function go_next(){
     setCount(count+1);
-    setName(scenario[count].name);
-    setText(scenario[count].text);
-    if(scenario[count].name==null){
+    setName(scenario.intro[count].name);
+    setText(scenario.intro[count].text);
+    if(scenario.intro[count].name==null){
       document.getElementById("script_name").style.visibility = "hidden";
     }else{
       document.getElementById("script_name").style.visibility = "visible";
@@ -40,7 +23,7 @@ function Novelview(props){
       <div id="script_name">{name}</div>
       <div id = "script_text">
         {text}
-        {count===scenario.length?<button onClick={()=>{props.final_next();}} id = "script_next">거래마치기</button>:<button onClick={()=>{go_next();}} id = "script_next">넘어가기</button>}
+        {count===scenario.intro.length?<button onClick={()=>{props.final_next();}} id = "script_next">거래마치기</button>:<button onClick={()=>{go_next();}} id = "script_next">넘어가기</button>}
         
         </div>
     </div>
