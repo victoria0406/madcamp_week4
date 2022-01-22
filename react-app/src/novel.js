47329efd6ee0119745805fdafa6a0b96a2924a30
Jonnie Yoon <yoonjeongin@sookmyung.ac.kr>
2022-01-22 13:10:18 +0900

    scenario

--- react-app/src/novel.js
+++ react-app/src/novel.js
@@ -3,29 +3,34 @@ import scenario from './scenario.json'
 
 import React, { Component, useEffect, useState } from 'react';
 
-function Novelview(props){
+
+function Novelview(props) {
   const [count, setCount] = useState(1);
   const [name, setName] = useState(scenario.intro[0].name);
   const [text, setText] = useState(scenario.intro[0].text);
 
-  function go_next(){
-    setCount(count+1);
+  function go_next() {
+    setCount(count + 1);
     setName(scenario.intro[count].name);
     setText(scenario.intro[count].text);
-    if(scenario.intro[count].name==null){
+    if (scenario.intro[count].name == null) {
       document.getElementById("script_name").style.visibility = "hidden";
-    }else{
+    } if (scenario.intro[count].name == "PLAYER") {
+      console.log("check player")
+      //document.getElementById("script_name").style.setText = "hi";
+    }
+    else {
       document.getElementById("script_name").style.visibility = "visible";
     }
   }
-  return(
+  return (
     <div class="novel">
       <div id="script_name">{name}</div>
-      <div id = "script_text">
+      <div id="script_text">
         {text}
-        {count===scenario.intro.length?<button onClick={()=>{props.final_next();}} id = "script_next">거래마치기</button>:<button onClick={()=>{go_next();}} id = "script_next">넘어가기</button>}
-        
-        </div>
+        {count === scenario.intro.length ? <button onClick={() => { props.final_next(); }} id="script_next">거래마치기</button> : <button onClick={() => { go_next(); }} id="script_next">넘어가기</button>}
+
+      </div>
     </div>
   );
 }
