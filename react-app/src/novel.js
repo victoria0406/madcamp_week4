7cb759fafaeb8d9ee45bcb027fb01047a0a176e5
Jonnie Yoon <yoonjeongin@sookmyung.ac.kr>
2022-01-24 04:29:35 +0900

    Add: Random lines in general trading

--- react-app/src/novel.js
+++ react-app/src/novel.js
@@ -1,5 +1,6 @@
 import './Game.css';
 import scenario from './scenario.json'
+import trade_scenario from './trade_scenario.json'
 import test from './test.json'
 import innerText from 'react-innertext'
 import SelectorView from './selector';
@@ -8,35 +9,42 @@ import React, { Component, useEffect, useState } from 'react';
 
 
 function Novelview(props) {
-  const [count, setCount] = useState(1);
-  let [name, setName] = useState(test[0].name);
-  const [text, setText] = useState(test[0].text);
-  const [type, setType] = useState(test[0].type);
-  const [user_name, setUsername] = useState(test[0].name);
-  //const user = props.user_name;
+
+  const shuffle = () => (Math.random() - 0.5);
+  const shuffled = [...trade_scenario].sort(shuffle);
+  //console.log("shuffled case", shuffled);
+
+
+  const [count, setCount] = useState(0);
+  let [name, setName] = useState(shuffled[0].case.name);
+  const [text, setText] = useState(shuffled[0].case.text);
+  const [type, setType] = useState(shuffled[0].case.type);
+  //const [user_name, setUsername] = useState(shuffled.case[0].name);
+
+  console.log("shuffled array[0]: ", shuffled[0].case[0])
 
   if (name == "Player") {
     name = props.user_name;
-    console.log("이름 바꾸기" + name + "\n user name: " + user_name);
+    //console.log("이름 바꾸기" + name + "\n user name: " + user_name);
   }
 
 
   function go_next() {
     setCount(count + 1);
-    setName(test[count].name);
-    setText(test[count].text);
-    setType(test[count].type);
+    setName(shuffled[0].case[count].name);
+    setText(shuffled[0].case[count].text);
+    setType(shuffled[0].case[count].type);
   }
 
   return (
 
     < div class="novel" >
-      {/* 이름, 텍스트 */}
-
       <div>
         {type == "select" ? <SelectorView /> : console.log("non select area!")}
       </div>
-      {name == null ? <></> : <div id="script_name"> { name } </div>}
+
+      {/* 이름, 텍스트 */}
+      {name == null ? <></> : <div id="script_name"> {name} </div>}
       <div id="script_text">
         {text}
         {count === scenario.length ? (
