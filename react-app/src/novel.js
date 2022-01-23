90a7642a2cf247bf7064123a9b264a4d5cde453b
Jonnie Yoon <yoonjeongin@sookmyung.ac.kr>
2022-01-23 17:56:27 +0900

    Fix: selector only in select case

--- react-app/src/novel.js
+++ react-app/src/novel.js
@@ -2,6 +2,7 @@ import './Game.css';
 import scenario from './scenario.json'
 import test from './test.json'
 import innerText from 'react-innertext'
+import SelectorView from './selector';
 
 import React, { Component, useEffect, useState } from 'react';
 
@@ -10,16 +11,19 @@ function Novelview(props) {
   const [count, setCount] = useState(1);
   let [name, setName] = useState(test[0].name);
   const [text, setText] = useState(test[0].text);
+  const [type, setType] = useState(test[0].type);
   const user = localStorage.getItem("user_id");
   const select = useState(test[4].select);
 
   console.log("select" + test[4].select)
-  let favor;
-  let fRavor;
 
   if (name == "김애저") {
     name = user;
     console.log("이름 바꾸기" + name);
+  }
+
+  if (type == "select") {
+
   }
 
   /*function go_next() {
@@ -42,27 +46,18 @@ function Novelview(props) {
     setCount(count + 1);
     setName(test[count].name);
     setText(test[count].text);
-    if (test[count] == "select") {
+    setType(test[count].type);
+    /*if (test[count] == "select") {
       document.getElementById('selector-wrapper').style.display = 'grid';
       document.getElementById('selectorA').innerHTML = "test plz"
-      /*document.getElementById('selector').innerHTML = test[count].map(i => 
+      document.getElementById('selector').innerHTML = test[count].map(i => 
         `<li onclick="addFavor('${encodeURI(JSON.stringify(i.variable))}')">${i.text}</li>`).join('')
     } else {
       document.getElementById('selectorwrapper').style.display = 'hidden';
-      document.getElementById('selector').style.display = 'hidden';*/
-    }
+      document.getElementById('selector').style.display = 'hidden';
+    }*/
   }
 
-  const selectCase = (v) => {
-    JSON.parse(decodeURI(v)).forEach(i => {
-      if (!favor[i.name]) {
-        favor[i.name] = i.cnt;
-      } else {
-        fRavor[i.name] += i.cnt;
-      }
-    })
-    ++count
-  }
 
   //선택지가 있으면 선택지를 출력 해 줍니다.
   /*if (!!select) {
@@ -71,29 +66,31 @@ function Novelview(props) {
       `<li onclick="selectCase('${encodeURI(JSON.stringify(i.variable))}')">${i.text}</li>`).join('')
   }*/
 
-  const isExist = props.isExist;
-
   return (
 
-        < div class="novel" >
-
+    < div class="novel" >
+      {/*
                       < div id="selector-wrapper" >
               <ul id="selector">
                 <li> test A </li>
                 <li> test B</li>
               </ul>
             </div >
+          */}
+      {/* 이름, 텍스트 */}
 
-          {/* 이름, 텍스트 */}
-          {name == null ? <></> : <div id="script_name">{name}</div>}
-          <div id="script_text">
-            {text}
-            {count === test.length ? <button onClick={() => { props.final_next(); }} id="script_next">거래마치기</button> : <button onClick={() => { go_next(); }} id="script_next">넘어가기</button>}
-          </div>
-        </div >
+      <div>
+        {type == "select" ? <SelectorView /> : console.log("non select area!")}
+      </div>
+      {name == null ? <></> : <div id="script_name">{name}</div>}
+      <div id="script_text">
+        {text}
+        {count === test.length ? <button onClick={() => { props.final_next(); }} id="script_next">거래마치기</button> : <button onClick={() => { go_next(); }} id="script_next">넘어가기</button>}
+      </div>
+    </div >
   );
-  }
+}
 
 
 
-      export default Novelview;
+export default Novelview;
