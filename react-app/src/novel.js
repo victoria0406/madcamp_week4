64c5c36fcfbb2a2f3436db92793300a2189a5ad8
victoria0406 <81007362+victoria0406@users.noreply.github.com>
2022-01-25 22:46:43 +0900

    나도 뭘 수정했는지 모르겠다

--- react-app/src/novel.js
+++ react-app/src/novel.js
@@ -4,15 +4,30 @@ import trade_scenario from "./scenario/trade_scenario.json";
 import test from "./scenario/test.json";
 import innerText from "react-innertext";
 
+import person1 from "./images/items/공기청정기.png";
+import person2 from "./images/items/청소기.png";
+import person3 from "./images/items/캡슐커피머신.png";
+import person4 from "./images/items/바람막이.jpg";
+
 import React, { Component, useEffect, useState } from "react";
+let person = person1;
 
 function randomValueFromArray(array) {
   const random = Math.floor(Math.random() * array.length);
+  if(random===1){
+    person = person2;
+  }else if(random==2){
+    person = person3;
+  }else if(random==3){
+    person = person4;
+  }
   return array[random];
 }
+
+let randomText = randomValueFromArray(trade_scenario);
 //const shuffle = () => (Math.random() - 0.5);
 //let shuffled = [...trade_scenario].sort(shuffle);
-let randomText = randomValueFromArray(trade_scenario);
+
 
 //console.log("random value from array: ", randomText.case[0]);
 //console.log("shuffle", shuffled);
@@ -20,13 +35,14 @@ let randomText = randomValueFromArray(trade_scenario);
 //console.log("test", test[0]);
 
 function Novelview(props) {
+  
   const [count, setCount] = useState(1);
   let [name, setName] = useState(randomText.case[0].name);
+  //const [person, setPerson] = useState(person1);
   const [text, setText] = useState(randomText.case[0].text);
   const [type, setType] = useState(randomText.case[0].type);
 
   if (name == "Player") {
-    name = props.user_name;
     //console.log("이름 바꾸기" + name + "\n user name: " + user_name);
   }
 
@@ -46,8 +62,10 @@ function Novelview(props) {
   //console.log("name: ", shuffled[0].case.name, "text: ", shuffled[0].case.text);
 
   return (
-    <div class="novel">
+      <div class="novel">
+      {count!==randomText.case.length&&<img className = "person_img" src = {person} alt="판매자임" height="500"/>}
       {name == null ? <></> : <div id="script_name">{name}</div>}
+      
       <div id="script_text">
         {text}
         {count === randomText.case.length ? (
@@ -64,6 +82,7 @@ function Novelview(props) {
         )}
       </div>
     </div>
+    
   );
 }
 
